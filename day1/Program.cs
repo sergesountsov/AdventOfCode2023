d52ac74a7e3acec8a430401b39cb99177de88af5
sergesountsov <serge.sountsov@gmail.com>
2023-12-01 16:56:42 +0000

    Part1 done

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,12 +1,20 @@
 ﻿string[] input = File.ReadAllLines("input.txt");
-ulong result = 0;
-foreach (string line in input) {
-    char[] digits = line.Where(Char.IsDigit).ToArray();
-    result += digits.Length switch {
-        0 => 0,
-        1 => Convert.ToUInt32(digits[0]),
-        _ => Convert.ToUInt32(digits[0]+digits[^1]),
-    };
-    
+
+Console.WriteLine($"part1 {Part1(input)}");
+
+static ulong Part1(string[] input)
+{
+    ulong result = 0;
+    foreach (string line in input)
+    {
+        char[] digits = line.Where(Char.IsDigit).ToArray();
+        ulong incr = digits.Length switch
+        {
+            0 => 0,
+            _ => Convert.ToUInt32($"{digits[0]}{digits[^1]}"),
+        };
+        result += incr;
+    }
+
+    return result;
 }
-Console.WriteLine($"part1 {result}");
