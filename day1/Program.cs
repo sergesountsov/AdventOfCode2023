63fa21e23059766117717ba011df378e0c8d05af
sergesountsov <serge.sountsov@gmail.com>
2023-12-01 16:36:22 +0000

    First stub at day1 part 1 solution

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = File.ReadAllLines("input.txt");
+ulong result = 0;
+foreach (string line in input) {
+    char[] digits = line.Where(Char.IsDigit).ToArray();
+    result += digits.Length switch {
+        0 => 0,
+        1 => Convert.ToUInt32(digits[0]),
+        _ => Convert.ToUInt32(digits[0]+digits[^1]),
+    };
+    
+}
+Console.WriteLine($"part1 {result}");
