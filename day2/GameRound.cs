6399b2ff5af07968acda6d0ede18027a8419876e
sergesountsov <serge.sountsov@gmail.com>
2023-12-03 00:55:55 +0000

    day 2 part 1

---
+++ day2/GameRound.cs
@@ -0,0 +1,33 @@
+public record GameRound (int Red, int Green, int Blue)
+{
+    public static GameRound Parse (string roundDescription) 
+    {
+        int red = 0; 
+        int green = 0; 
+        int blue = 0;
+        
+        var colors = roundDescription.Split(',');
+        foreach (string color in colors) 
+        {
+            var tokens = color.Trim().Split(' ');
+            switch (tokens[1]) 
+            {
+                case "red": red = int.Parse(tokens[0]); break;
+                case "blue": blue = int.Parse(tokens[0]); break;
+                case "green": green = int.Parse(tokens[0]); break;
+            }
+        }
+        return new GameRound(red,green,blue);
+    }
+
+    public static List<GameRound> ParseGame (string gameDescription) 
+    {
+        List<GameRound> result = new();
+        string[] rounds = gameDescription.Split(':')[1].Trim().Split(';');
+        foreach (string round in rounds) 
+        {
+            result.Add(GameRound.Parse(round.Trim()));
+        }
+        return result;
+    }
+}
---
+++ day2/Program.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+public class Day2 
+{
+    [Fact]
+    void Part1() 
+    {
+        string[] input = File.ReadAllLines("input.txt");
+        List<List<GameRound>> games = new();
+        foreach (string gameDescr in input) 
+        {
+            games.Add(GameRound.ParseGame(gameDescr));
+        }
+
+        Assert.Equal(100, games.Count);
+
+        int result = 0;
+        for(int i = 0; i < games.Count; i++) 
+        {
+            if (games[i].Count(round => round.Red > 12 || round.Green > 13 || round.Blue > 14) == 0) 
+            {
+                result += i + 1;
+            }
+        }
+
+        Console.WriteLine($"part1 {result}");
+    }
+
+    [Fact]
+    void Parse_One_GameRound() 
+    {
+        GameRound actual = GameRound.Parse("3 red, 5 green, 4 blue");
+        Assert.Equal(new GameRound(3,5,4), actual);
+    }
+
+    [Fact]
+    void Parse_One_Color()
+    {
+        GameRound actual = GameRound.Parse("3 green");
+        Assert.Equal(new GameRound(0,3,0), actual);
+    }
+
+    [Fact]
+    void ParseGame_OneRound() 
+    {
+        var actual = GameRound.ParseGame("Game 3: 12 green, 2 blue");
+        Assert.Equal(new List<GameRound> {new GameRound(0,12,2)}, actual);
+    }
+
+    [Fact]
+    void Parse_MultipleRounds() 
+    {
+        var actual = GameRound.ParseGame("Game 2: 3 green, 18 blue; 14 green, 4 red, 2 blue; 3 red, 14 green, 15 blue");
+        Assert.Equal(new List<GameRound> {new GameRound(0,3,18), new GameRound(4,14,2), new GameRound(3,14,15)}, actual);
+    }
+
+}
+
